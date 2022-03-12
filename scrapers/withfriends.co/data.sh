0cf689e24a177a8a56337748a20618eac0c27b34
fabacab <fabacab@riseup.net>
2022-03-12 11:46:27 -0500

    Also scrape Babycastles events.

--- scrapers/withfriends.co/data.sh
+++ scrapers/withfriends.co/data.sh
@@ -4,6 +4,7 @@ local base_url="https://withfriends.co"
 
 # "Movements" in Withfriend.co's bizarre "Jelly" CMS.
 local movement_ids=(
+    105694 # Babycastles
     250678 # Mayday Space
     801694 # The Footlight Underground at The Windjammer
     829674 # Bluestockings Activist Bookstore and Cafe
